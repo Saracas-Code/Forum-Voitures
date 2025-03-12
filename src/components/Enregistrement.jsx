9d9250136ce7c4479da73f314c56a2e2ec41f448
Sara Castro <saracas04@gmail.com>
2025-03-12 12:57:00 +0100

    Modificadas Login y Enregistrement. Añadido comportamiento js

--- src/components/Enregistrement.jsx
+++ src/components/Enregistrement.jsx
@@ -1,34 +1,88 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import "../styles/forms.css"
+import "../styles/Enregistrement.css"
 
 const Enregistrement = () => {
+
+    // States pour les inputs et le message d'erreur
+    const [prenom, setPrenom] = useState("");
+    const [nom, setNom] = useState("");
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+    const [retapez, setRetapez] = useState("");
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
+    const navigate = useNavigate(); // Pour rédiriger après le login
+
+    // Fonction pour vérifier la registration
+    const handleSubmit = (event) => {
+        event.preventDefault(); // Éviter que la page se recharge
+
+        if (!prenom || !nom || !login || !password || !retapez) {
+            setError("Veuillez remplir tous les champs");
+            setSuccess("");
+            return;
+        }
+
+        if (password !== retapez) {
+            setError("La password n'est pas la même");
+            setSuccess("");
+            return;
+        }
+
+        setSuccess("Inscription réussie! En attente de validation administrative ")
+        setError("");
+        
+        // APRÈS ÇA, L'USER DOIT ATTENDRE À QU'UN ADMIN LUI ACEPTE ET L'AJOUTE DANS LA BASE DE DONNÉES
+    };
+
+    const handleReset = () => {
+        setPrenom("");
+        setNom("");
+        setLogin("");
+        setPassword("");
+        setRetapez("");
+        setError("");
+        setSuccess("");
+    };
+
     return (
-        <div>
+        <div className="enregistrement-container">
             <h1>Enregistrement</h1>
             <div id="connect">
                 <Link to="/">Page Principale</Link>
                 <Link to="/login">Connexion</Link>
             </div>
-            <form>
+            <form onSubmit={handleSubmit} onReset={handleReset}>
                 <div className="prenom_nom">
-                    <label htmlFor="chp_prenom">Prénom</label>
-                    <input id="chp_prenom" placeholder="Name..." />
-
-                    <label htmlFor="chp_nom">Nom</label>
-                    <input id="chp_nom" placeholder="Surname..." />
+                    <div>
+                        <label htmlFor="chp_prenom">Prénom</label>
+                        <input id="chp_prenom" placeholder="Name..." value={prenom} onChange={(e) => setPrenom(e.target.value)} />
+                    </div>
+                    <div>
+                        <label htmlFor="chp_nom">Nom</label>
+                        <input id="chp_nom" placeholder="Surname..." value={nom} onChange={(e) => setNom(e.target.value)} />
+                    </div>
                 </div>
 
-                <label htmlFor="chp_login">Login</label>
-                <input id="chp_login" placeholder="Login..." />
+                <label htmlFor="chp_login">Login (Username)</label>
+                <input id="chp_login" placeholder="Login..." value={login} onChange={(e) => setLogin(e.target.value)}/>
 
                 <label htmlFor="chp_password">Mot de passe</label>
-                <input type="password" id="chp_password" placeholder="Password..." />
+                <input type="password" id="chp_password" placeholder="Password..." value={password} onChange={(e) => setPassword(e.target.value)}/>
 
-                <label htmlFor="chp_retapez">Retapez</label>
-                <input type="password" id="chp_retapez" placeholder="Repeat password..." />
+                <label htmlFor="chp_retapez">Retapez le mot de passe</label>
+                <input type="password" id="chp_retapez" placeholder="Repeat password..." value={retapez} onChange={(e) => setRetapez(e.target.value)}/>
 
                 <button type="submit">S'inscrire</button>
                 <button type="reset">Annuler</button>
+
+                {error && <p className="error">{error}</p>}
+                {success && <p className="success">{success}</p>}
             </form>
         </div>
     );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,42 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import "../styles/forms.css"
 import "../styles/Login.css"
 
 const Login = () => {
+
+    // States pour les inputs et le message d'erreur
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const navigate = useNavigate(); // Pour rédiriger après le login
+
+    // Fonction pour vérifier l'authentification
+    const handleSubmit = (event) => {
+        event.preventDefault(); // Éviter que la page se recharge
+
+        if (!login || !password) {
+            setError("Veuillez remplir tous les champs.");
+            return;
+        }
+
+        // DEMANDE À LA DATABASE SI L'USER EXISTE OU PAS. Pour l'instant, simuler l'authentification
+        if (login === "sara" && password === "cas") {
+            navigate("/"); // On va au Forum publique
+        } else {
+            setError("Login ou mot de passe incorrect"); 
+        }
+    };
+
+    const handleReset = () => {
+        setLogin("");
+        setPassword("");
+        setError("");
+    };
+
     return (
         <div className="login-container">
             <h1>Ouvrir une session</h1>
@@ -10,15 +44,18 @@ const Login = () => {
                 <Link to="/">Page Principale</Link>
                 <Link to="/register">Enregistrement</Link>
             </div>
-            <form>
+            <form onSubmit={handleSubmit} onReset={handleReset}>
+
                 <label htmlFor="chp_login">Login</label>
-                <input id="chp_login" placeholder="Login..." />
+                <input id="chp_login" placeholder="Login..." value={login} onChange={(e) => setLogin(e.target.value)} />
 
                 <label htmlFor="chp_password">Mot de passe</label>
-                <input type="password" id="chp_password" placeholder="Password..." />
+                <input type="password" id="chp_password" placeholder="Password..." value={password} onChange={(e) => setPassword(e.target.value)} />
 
                 <button type="submit">Connexion</button>
                 <button type="reset">Annuler</button>
+
+                {error && <p className="error">{error}</p>}
             </form>
         </div>
     );
